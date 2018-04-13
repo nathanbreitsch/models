2b54a1b86cba647b73402d3a5cabb5031246764d
Nathan Breitsch <nathanbreitsch@gmail.com>
2018-04-13 13:46:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 """Setup script for object_detection."""
 
-from setuptools import find_packages
 from setuptools import setup
-
+from setuptools import find_packages
 
 REQUIRED_PACKAGES = ['Pillow>=1.0']
 
@@ -11,6 +10,16 @@ setup(
     version='0.1',
     install_requires=REQUIRED_PACKAGES,
     include_package_data=True,
-    packages=['object_detection', 'slim'],
+    packages=[p for p in find_packages() if p.startswith('object_detection')],
+    description='Tensorflow Object Detection Library',
+)
+
+
+setup(
+    name='slim',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    packages=[p for p in find_packages() if p.startswith('slim')],
     description='Tensorflow Object Detection Library',
 )
